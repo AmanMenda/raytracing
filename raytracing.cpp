83bbb6a8b5ca373a8441f914dc025e622b6e0828
Aman Menda <aman.menda@epitech.eu>
2025-06-18 16:49:39 +0200

    init repository

---
+++ raytracing.cpp
@@ -0,0 +1,12 @@
+﻿// raytracing.cpp : définit le point d'entrée de l'application.
+//
+
+#include "raytracing.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
